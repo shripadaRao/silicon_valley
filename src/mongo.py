751041e1d3cd8dd41b60425cfa37260335223ec2
Shripad Rao <shripada0987@gmail.com>
2022-08-06 19:36:40 +0530

    pushed data to db

---
+++ src/mongo.py
@@ -0,0 +1,32 @@
+"""THIS FILE GETS DATA FROM data_transformation.py & PUSHES RECORDS INTO MONGODB"""
+
+import data_transform
+from pymongo import MongoClient
+
+with open('env.txt','r') as f:
+    MONGO_URI = f.read()
+
+client = MongoClient(MONGO_URI)
+db = client["SiliconValley"]
+col = db["Quotes"]
+
+DataModel = {"_id":1, "character_name":"", "quote":""}         
+
+def make_record(i):
+    DataModel["_id"] = i
+    DataModel["character_name"] = data_transform.character_name_list[i]
+    DataModel["quote"] = data_transform.quotes_list[i]
+    i+=1
+    return DataModel
+
+def push_record(data):
+    col.insert_one(data)
+
+def main():
+    for i in range(len(data_transform.character_name_list)):
+        make_record(i)
+        push_record(DataModel)
+        print("Pushing record!!")
+    print("Done")
+
+main()
