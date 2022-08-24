5f609451e06278a520341369fa25d2caabfc667f
Shripad Rao <shripada0987@gmail.com>
2022-08-24 16:17:28 +0530

    changes to character_quotes api

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,52 @@
 from http import client
 from flask import Flask
 from flask_pymongo import pymongo
+from markupsafe import escape
 from flask import jsonify
 
 app = Flask(__name__)
 
-conn = "mongodb+srv://shripad_rao:MfAPwUqXG9IpHXJa@cluster0.cqtw61h.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(conn)
-db = client["SiliconValley"]
-col = db['Quotes']
+def col():
+    conn = open("env.txt", 'r').read()
+    print(conn)
+    client = pymongo.MongoClient(conn)
+    db = client["SiliconValley"]
+    col = db['QuotesFinal']
+    return col
 
+###-------------------------------------------------------------------------------------------###
 
 @app.route('/')
 def flask_mongo_db():
     return("hello, this is homepage")
 
-@app.route('/quotes/<character_name>')
-def findCharacterName1(character_name):
+@app.route('/quotes/<character_name>')              #Print all quotes from the given character. 
+def findCharacterName(character_name):
     myquery = {"character_name":character_name}
     DataList = []
-    for x in col.find(myquery):     
+    contentList = []
+    for x in col().find(myquery):     
         DataList.append(x)
+        contentList.append(x["quote"])
     if not DataList:
-        return("Wrong Charater")
-    return(DataList)
+        return("Wrong Character")
+    return(jsonify(contentList))
 
 @app.route('/quotes/all')
 def findAll():
     DataList = []
-    for x in col.find({}):
+    for x in col().find({}):
         DataList.append(x)
-    return(DataList)
+    return(jsonify(DataList))
+
+@app.route('/quotes/character-names') #Print all character names
+def findCharacterNames():
+    c_names = []
+    for x in col().find({},{"character_name":1}):
+        c_names.append(x["character_name"])
+    
+    character_names = set(c_names)
+    return((list(character_names)))
     
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
